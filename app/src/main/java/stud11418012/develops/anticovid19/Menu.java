72fe5ba2783ad23790e3c409c4c9f7903492b78f
jessicasiburian <jessicasiburian969@gmail.com>
2020-03-28 14:54:22 +0700

    mengubah splashscreen

--- app/src/main/java/stud11418012/develops/anticovid19/Menu.java
+++ app/src/main/java/stud11418012/develops/anticovid19/Menu.java
@@ -1,15 +1,15 @@
 package stud11418012.develops.anticovid19;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Menu extends AppCompatActivity {
 
     @Override
-    public boolean onCreateOptionsMenu(android.view.Menu menu){
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-
-
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
     }
 
 
