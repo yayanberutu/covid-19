700630c87bec45796c433014f531040a124f51f8
jessicasiburian <jessicasiburian969@gmail.com>
2020-03-28 18:05:26 +0700

    jeje

--- app/src/main/java/stud11418012/develops/anticovid19/Menu.java
+++ app/src/main/java/stud11418012/develops/anticovid19/Menu.java
@@ -1,6 +1,8 @@
 package stud11418012.develops.anticovid19;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +11,23 @@ public class Menu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_riwayat);
     }
-
+        //public boolean onCreateOptionMenu(Menu menu){
+          //  MenuInflater inflater = getMenuInflater();
+            //inflater.inflate(R.menu.menu_main, menu);
+            //return true;
+        //}
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId()==R.id.bio){
+            startActivity(new Intent(this, Biodata.class));
+        }else if (item.getItemId()== R.id.about){
+            startActivity(new Intent(this, About.class));
+        }else if (item.getItemId()==R.id.uji){
+            startActivity(new Intent(this, Pengujian.class));
+        }
+        return true;
+    }
 
 
 }
--- app/src/main/java/stud11418012/develops/anticovid19/Splashscreen.java
+++ app/src/main/java/stud11418012/develops/anticovid19/Splashscreen.java
@@ -2,6 +2,7 @@ package stud11418012.develops.anticovid19;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +15,14 @@ public class Splashscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), Riwayat.class));
+                finish();
+            }
+        }, 3000);
 
     btnCek = findViewById(R.id.button1);
         btnCek.setOnClickListener(new View.OnClickListener() {
